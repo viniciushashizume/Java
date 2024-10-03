5b1b7efc5bc88aa46c250b133b4126523b449740
Vinicius Hashizume <viniciushashizume26@gmail.com>
2024-10-02 21:30:08 -0300

    Create EX04.java

---
+++ JavaPOO/ProjetoPOO/src/projetos/EX04.java
@@ -0,0 +1,21 @@
+package projetos;
+
+public class EX04 {
+    public static class pessoa{
+        String nome;
+        int idade;
+        double peso;
+        public void print()
+        {
+            System.out.println("Nome: " + nome + " Idade: " + idade + " Peso: " + peso);
+        }
+    }
+   public static void main(String[] args)
+   {
+      pessoa obj = new pessoa();
+      obj.nome = "AAAAAA";
+      obj.idade = 15;
+      obj.peso = 11.3;
+      obj.print();
+   } 
+}
